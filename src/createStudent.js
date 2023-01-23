62af5b5413b1697abb156b8bcca8f929c95fc7fd
Vinícius Souza <tradutorvinicius@gmail.com>
2023-01-23 18:03:33 -0300

    Resolve requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const studentObject = {
+    name,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return studentObject;
+};
 
 module.exports = createStudent;
