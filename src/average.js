61c749fb4d98d268598731d373fd89a86d5d6055
Vinícius Souza <tradutorvinicius@gmail.com>
2023-01-23 15:26:43 -0300

    Resolve requisito 1

--- src/average.js
+++ src/average.js
@@ -11,6 +11,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  if (array.length < 1) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== "number") {
+      return undefined;
+    }
+    sum += array[index];
+  }
+  return Math.round(sum / array.length);
+};
 
 module.exports = average;
