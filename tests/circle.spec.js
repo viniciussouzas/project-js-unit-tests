1ff1a382eb68f9416226ca80c032dca28773ff88
Vinícius Souza <tradutorvinicius@gmail.com>
2023-01-23 17:51:56 -0300

    Resolve requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 
+const { TestScheduler } = require('jest');
 const circle = require('../src/circle');
 
 /*
@@ -23,14 +24,19 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
-    // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado possui 3 propriedades.
-    // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
-    // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
-    // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
+    
+      expect(circle('xablau')).toBe(undefined);
+
+      expect( typeof circle(1)).toBe('object');
+
+      expect(Object.keys(circle(1)).length).toBe(3);
+
+      expect(circle()).toBe(undefined);
+
+      expect(circle(1)).toHaveProperty('circumference', 6.28);
+
+      expect(circle(7)).toHaveProperty('area', 153.86);
+
+      expect(circle(3)).toEqual({radius: 3, area: 28.259999999999998, circumference: 18.84});
   });
 });
