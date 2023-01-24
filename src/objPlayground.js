e1cdd9a5fb37af43d1b43435cdc2d2b3735769e4
Vinícius Souza <tradutorvinicius@gmail.com>
2023-01-24 15:40:14 -0300

    Resolve requisito 7

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,15 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
-
-const arrayGenerator = (type, object) => {};
+const calculator = (number1, number2) => {
+  const object = {
+    sum: number1 + number2,
+    mult: number1 * number2,
+    div: Math.floor(number1 / number2),
+    sub: number1 - number2,
+  };
+  return object;
+};
+const arrayGenerator = (type, object) => Object[type](object);
 
 module.exports = { calculator, arrayGenerator };
